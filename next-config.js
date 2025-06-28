471b9511a1e523c69ffdff66abbf746f41399ba4
Yadongofficial <35225627+YadongOfficial@users.noreply.github.com>
2025-06-28 15:57:26 +0700

    Create next-config.js

---
+++ next-config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true, // Optional: enables faster builds
+};
+
+module.exports = nextConfig;
+ 
